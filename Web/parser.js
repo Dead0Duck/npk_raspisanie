1d49e74de73fc530da97cbad779b766c1e60f8f1
DeadDuck <46423255+Dead0Duck@users.noreply.github.com>
2023-02-26 23:26:26 +0700

    feat: Теперь парсер ещё устойчивее к изменениям в структуре расписания

--- Web/parser.js
+++ Web/parser.js
@@ -52,14 +52,21 @@ async function ParseRaspisanie(sql, webPush)
 				const dataWS = jObj.Sheets[jObj.SheetNames[0]]
 				const data = XLSX.utils.sheet_to_json(dataWS, {header: 1})
 
+				let y_start = 0
+				for(let i = 0; i < 10; i++)
+				{
+					if(data[i][1] == "№")
+					y_start = i
+				}
+
 				let groups = {}
-				for(let i = 5; i < data[5].length; i+=5) {
-					if(data[5][i])
-						groups[data[5][i]] = i
+				for(let i = 5; i < data[y_start].length; i+=5) {
+					if(data[y_start][i])
+						groups[data[y_start][i]] = i
 				}
 
 				let para_count = 5;
-				for(let i = 8; i < 17; i++) {
+				for(let i = y_start + 3; i < y_start + 12; i++) {
 					if(data[i][0])
 					{
 						para_count = (i-7)/2
@@ -69,51 +76,59 @@ async function ParseRaspisanie(sql, webPush)
 
 				Object.entries(groups).forEach(([group, group_pos]) => {
 					for(let day_ind = 0; day_ind < 60; day_ind += (para_count*2)) {
-						let day = data[7 + day_ind]
+						let day = data[y_start + 2 + day_ind]
 						if(!day || !day[0]) 
 							break
 
-						day = day[0].split("  ")[0] // Получаем дату из дня
+						day = day[0].match(`(0[1-9]|[1-2][0-9]|3[0-1])\.(0[1-9]|1[0-2])\.[0-9]{4}`)[0] // Получаем дату из дня
+						console.log(day)
 
 						// Формируем объект расписания
 						raspisanie[day] = raspisanie[day] || {}
 						raspisanie[day][group] = raspisanie[day][group] || []
 
-						for(let i = 7 + day_ind; i < ((para_count*2 + 6) + day_ind); i += 2)
+						for(let i = 0; i < (para_count * 2); i += 2)
 						{
-							let predmet = data[i][group_pos]
-							let prepod = data[i+1][group_pos]
-							let cabinet = data[i][group_pos+3]
+							let y = y_start + 2 + day_ind + i
+							let predmet = data[y][group_pos]
+							let prepod = data[y + 1][group_pos]
+							let cabinet = data[y][group_pos+3]
 
-							if(!predmet) {
+							if(!predmet && !data[y][group_pos+2]) {
 								raspisanie[day][group].push(false)
 								continue
 							}
 
-							if(data[i][group_pos+1]) {
+							if(data[y][group_pos+1]) {
 								// Если предмет поделён на две группы
 								let ind = raspisanie[day][group].push([]) - 1
 
 								// Пушим первую группу
-								cabinet = data[i][group_pos+1]
-								raspisanie[day][group][ind].push({
-									predmet,
-									prepod,
-									cabinet,
-									group: 1
-								})
+								if(predmet)
+								{
+									cabinet = data[y][group_pos+1]
+									raspisanie[day][group][ind].push({
+										predmet,
+										prepod,
+										cabinet,
+										group: 1
+									})
+								}
 
 								// Пушим вторую группу
-								predmet = data[i][group_pos+2]
-								prepod = data[i+1][group_pos+2]
-								cabinet = data[i][group_pos+3]
+								predmet = data[y][group_pos+2]
+								prepod = data[y + 1][group_pos+2]
+								cabinet = data[y][group_pos+3]
 
-								raspisanie[day][group][ind].push({
-									predmet,
-									prepod,
-									cabinet,
-									group: 2
-								})
+								if(predmet)
+								{
+									raspisanie[day][group][ind].push({
+										predmet,
+										prepod,
+										cabinet,
+										group: 2
+									})
+								}
 							} else {
 								// Одна группа и не паримся
 								raspisanie[day][group].push({
