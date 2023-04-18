09b315ed14f447cd65710ba98bf4c71b811a5485
DeadDuck <46423255+Dead0Duck@users.noreply.github.com>
2023-04-18 15:53:13 +0700

    🎨 Рефакторинг под авто-апдейт

--- index.js
+++ index.js
@@ -1,67 +1,18 @@
-module.exports = { run: () => {
-
-const Notifications = require('./app_modules/notifications')
-
 require('dotenv').config();
 
-const express = require('express');
-const app = express();
-const https = require('https');
-const opt_cert = {
-	key: process.env.SSL_PRIVKEY,
-	cert: process.env.SSL_FULLCHAIN
-};
-const server = https.createServer(opt_cert, app);
-const webPush = require('web-push');
-const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
-const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
-
-webPush.setVapidDetails('https://'+process.env.URL, publicVapidKey, privateVapidKey);
-
-const RaspisanieParser = require('./app_modules/parser');
-RaspisanieParser.start(Notifications, webPush)
-
-app.use(express.json());
-
-// Подключение Helmet для увеличения безопасносности
-const helmet = require('helmet')
-const helmet_settings = {
-	contentSecurityPolicy: false,
-	crossOriginEmbedderPolicy: false,
-	crossOriginResourcePolicy: false,
+const AutoGitUpdate = require('auto-git-update');
+const config = {
+	repository: process.env.GIT_URL,
+	tempLocation: '../RaspisanieAutoUpd/',
+	branch: process.env.GIT_BRANCH,
+	ignoreFiles: [],
+	executeOnComplete: 'npm run start',
+	exitOnComplete: true,
+	token: process.env.GIT_TOKEN,
 }
-app.use(helmet(helmet_settings));
 
-// Защита от обращений по IP
-app.all("*", (req, res, next) => {
-	if(req.headers.host == process.env.URL)
-		return next();
+const updater = new AutoGitUpdate(config);
+updater.autoUpdate();
 
-	res.sendStatus(403);
-})
-
-
-app.use(express.static('./site/resources'));
-
-app.get('/api/get', (req, res, next) => {
-	res.json(RaspisanieParser.get())
-})
-
-app.post('/subscribe', (req, res) => {
-	const subscription = req.body
-
-	Notifications.add({
-		"p256dh": subscription.keys.p256dh,
-		"auth": subscription.keys.auth,
-		"endpoint": subscription.endpoint
-	})
-  
-	res.status(201).json({});
-});
-  
-const port = process.env.PORT || 443
-server.listen(port, () => {
-	console.log(`Сайт запущен на порте ${port}`)
-});
-
-}}
+setTimeout( () => require('./Site').run(), 2000 )
+setInterval( () => updater.autoUpdate(), 1000 * 60 * 60 ) // Проверка на обновления каждый час
